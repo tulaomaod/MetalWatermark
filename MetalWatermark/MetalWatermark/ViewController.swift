d2a5d5a5b8ccd7d1c258b9fc19b5f2b016de3765
mac126 <tulaomao152@126.com>
2018-05-31 16:39:24 +0800

    initial commit

---
+++ MetalWatermark/MetalWatermark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MetalWatermark
+//
+//  Created by mac126 on 2018/5/31.
+//  Copyright © 2018年 mac126. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
